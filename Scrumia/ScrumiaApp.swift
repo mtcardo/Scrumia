64aa3c8e2562de18893b3d7dbfc9d361cbd61fd4
Mateus Cardoso <mtcardo@icloud.com>
2022-11-07 20:52:59 -0300

    Initial Commit

---
+++ Scrumia/ScrumiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumiaApp.swift
+//  Scrumia
+//
+//  Created by Mateus Cardoso on 07/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
